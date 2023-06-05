a75b5118b2178df6c31be2fb23caee94fb0e7526
Kristjan Poska <kristjan.poska@guardtime.com>
2023-06-05 18:02:41 +0300

    Compress images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
+  overrides: [{
+    files: '*.astro',
+    options: {
+      parser: 'astro'
+    }
+  }]
 });
