f5bd4564f6ff25d827e7adf53fb11652c25d9ce1
Kristjan Poska <kristjan.poska@guardtime.com>
2023-06-07 18:05:38 +0300

    Add mobile nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
     screens: {
-      '2xl': {'max': '1535px'},
-      'xl': {'max': '1279px'},
-      'lg': {'max': '1023px'},
-      'md': {'max': '767px'},
-      'sm': {'max': '639px'},
-    }
-	},
-	plugins: [],
-}
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+      "min-sm": { min: "640px" },
+    },
+  },
+  plugins: [],
+};
