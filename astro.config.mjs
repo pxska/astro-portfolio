1c4f4054daa1d4dad416a5ad679b01f936e44285
Kristjan Poska <kristjan.poska@guardtime.com>
2023-07-24 10:17:48 +0300

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  adapter: vercel({
+    analytics: true,
+  }),
   integrations: [tailwind()],
-  plugins: ['prettier-plugin-astro'],
-  overrides: [{
-    files: '*.astro',
-    options: {
-      parser: 'astro'
-    }
-  }]
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 });
