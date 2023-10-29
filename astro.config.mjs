7d2073a7e06654b19c2fda5e762123d9f2d85d91
pxska <kristjanposka@gmail.com>
2023-10-29 17:07:05 +0200

    Add MDX to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   adapter: vercel({
-    webAnalytics: true,
+    webAnalytics: true
   }),
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+  overrides: [{
+    files: "*.astro",
+    options: {
+      parser: "astro"
+    }
+  }]
 });
