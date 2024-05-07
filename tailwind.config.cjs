c537f318a8f97231da90b01105e3c57c4eb529b1
pxska <kristjanposka@gmail.com>
2024-05-07 20:58:03 +0300

    Remove unused code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
     extend: {
-      scale: {
-        101: "1.01",
+      colors: {
+        "theme-orange": "#F27059",
       },
     },
     screens: {
